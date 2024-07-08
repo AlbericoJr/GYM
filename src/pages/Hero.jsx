c868ea1252065457566a355770e6e3f58ff8c462
AlbericoJr <alberico14@live.com>
2024-07-08 18:48:50 -0300

    GYM

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -66,7 +66,7 @@ const Hero = () => {
                 height: "100vh",
               }}
             >
-              <div className="w-full h-full bg-black/60 absolute top-0 left-0 -z-10"></div>
+              <div className="w-full h-full bg-black/70 absolute top-0 left-0 -z-10"></div>
               <div className="w-full h-full flex items-center justify-center flex-col z-10 lg:px-24 md:px-16 sm:px-6 px-4">
                 <h5 className="lg:text-4xl md:text-3xl sm:text-2xl text-2xl text-orange-500 font-semibold mb-2 uppercase">
                   {data.slogan}
